5e3d50b67d62fb13f676a1259452d0e11af82621
Joann Monteiro <joannjoymonteiro2021@gmail.com>
2026-07-16 17:48:41 -0400

    Initial Commit

---
+++ SliceMaster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SliceMaster
+//
+//  Created by Joann Monteiro on 2026-07-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
